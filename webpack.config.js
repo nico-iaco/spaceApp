e88451eb8fbbc525ceecf302224553260ec1a7df
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2021-10-04 11:28:47 +0200

    updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,13 @@ module.exports = {
             test: /\.s?css$/
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    mode: "development",
+    devtool: 'cheap-module-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
     }
 };
